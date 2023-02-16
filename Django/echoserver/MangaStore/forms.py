77c4a52af27365d27ac2d12a1ae058f2eec2c02e
Belphisto <100871374+Belphisto@users.noreply.github.com>
2023-02-16 00:57:03 -0800

    Delete Django/echoserver directory

--- Django/echoserver/MangaStore/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MangaForm(forms.Form):
-    name = forms.CharField(max_length=30, label="Manga", initial="BaseName", required=True)
-    author = forms.CharField(max_length=30, label="Author", initial="BaseAuthorName", required=True)
-    price = forms.FloatField(min_value = 0, initial=0.0, help_text="enter a non-negative number", required=True)
--- Django/echoserver/MangaStore/models.py
+++
@@ -1,9 +0,0 @@
-from statistics import mode
-from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-# Create your models here.
-class Manga(models.Model):
-    name = models.CharField(max_length=30)
-    author = models.CharField(max_length=30)
-    price = models.FloatField(validators=[MinValueValidator(0)])
